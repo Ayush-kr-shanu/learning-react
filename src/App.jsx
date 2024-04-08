6dba55f9f1c86bae13265447daa12728cd32e430
Ayush-kr-shanu <aksmilestone35@gmail.com>
2024-04-08 10:24:35 +0530

    setting react and learning

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Greeting from './components/greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ayush Project</h1>
+      <Greeting name="Priya" />
+    </div>
+  );
+}
+
+export default App;
